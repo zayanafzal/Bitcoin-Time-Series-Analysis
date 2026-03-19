90e3f6a53324af475448a16f08cdba077c643c2b
zayanafzal <150970979+zayanafzal@users.noreply.github.com>
2026-03-19 23:10:09 +0000

    Add files via upload

---
+++ Coursework1_220242406.R
@@ -0,0 +1,61 @@
+#' title: Time Series CW1
+#' author: Zayan Afzal
+#' date: 13/03/2026
+
+# Installing Project ------------------------------------------------------
+
+install.packages("prophet")
+library(prophet)
+
+# Loading in my dataset (BitcoinTimeSeries) -------------------------------
+
+data = read.csv("BitcoinTimeSeries.csv")
+head(data)
+names(data)
+str(data)
+
+# Setting up dataframe -----------------------------------------------------------------
+
+bitcoin = data.frame(
+    ds = as.Date(data$Time, format="%d/%m/%Y"),
+    y = data$Bitcoin
+)
+
+
+# Checking if names updated -----------------------------------------------
+
+head(bitcoin)
+names(bitcoin)
+str(bitcoin)
+
+# Plotting ----------------------------------------------------------------
+
+plot(bitcoin$ds, bitcoin$y,
+     type="l",
+     xaxt="n",
+     xlab="Year",
+     ylab="Search Interest",
+     main="Google Search Trends for Bitcoin")
+
+
+axis.Date(1,
+          at=seq(min(bitcoin$ds),
+                 max(bitcoin$ds),
+                 by="1 year"),
+          format="%Y")
+
+
+# Prophet Forecast & Plot -------------------------------------------------
+
+model = prophet(bitcoin)
+future = make_future_dataframe(model,
+                               periods=24,
+                               freq="month")
+
+forecast = predict(model, future)
+plot(model, forecast)
+
+
+# Plotting Trend and Seasonality ------------------------------------------
+prophet_plot_components(model, forecast)
+
